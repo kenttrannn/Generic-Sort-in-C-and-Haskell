a5aa47c3b931bae3f94a56d5d51cf4817e0d2677
Kent Tran <kent.tran2219@gmail.com>
2026-03-23 16:46:49 -0700

    Add files via upload

--- generic_sort.cpp
+++ generic_sort.cpp
@@ -20,14 +20,26 @@ struct Person
     int age;
 };
 
+bool sortNames(const Person &a, const Person &b)
+{
+    return a.name < b.name;
+}
+
+bool sortAges(const Person &a, const Person &b)
+{
+    if (a.age != b.age)
+    {
+        return a.age < b.age;
+    }
+    return a.name < b.name;
+}
+
 int main()
 {
     vector<double> numbers =
     {
         645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25
     };
-    // gets total byte / 4(byte of first array only)
-    int num_count = sizeof numbers / sizeof numbers[0];
 
     vector<Person> people = 
     {
@@ -37,32 +49,18 @@ int main()
         {"Danna", 20}, {"Zara", 23}, {"Rosalyn", 26},
         {"Risa", 24}, {"Benny", 28}, {"Juan", 33}, {"Natalie", 25}
     };
-    int people_count = sizeof(people) / sizeof(people[0]);
-
 
     //Sort numbers in ascending order by numerical value.
     vector<double> sortedNums = numbers;
-    sort(sortedNums.begin(), sortedNums.end(),
-    [](const Person &a, const Person &b)
-    {
-        return a.name < b.name;
-    });
+    sort(sortedNums.begin(), sortedNums.end());
 
     //Sort people alphabetically (lexicographically) by name.
     vector<Person> sortedNames = people;
-    sort(sortedNames.begin(), sortedNames.end());
+    sort(sortedNames.begin(), sortedNames.end(), sortNames);
 
     //Sort people in descending order by age. If two people have the same age, sort them alphabetically by name.
-    vector<Person> sortAges = people;
-    sort(sortAges.begin(), sortAges.end(),
-    [](const Person &a, const Person &b) 
-    {
-        if (a.age != b.age)
-        {
-            return a.age > b.age;
-        }
-        return a.name < b.name;
-    });
+    vector<Person> sortedAges = people;
+    sort(sortedAges.begin(), sortedAges.end(), sortAges);
 
     cout << "Sorted numbers: " << endl;
     for (double n : sortedNums)
@@ -70,14 +68,14 @@ int main()
         cout << fixed << setprecision(2) << n << endl;
     }
 
-    cout << "Sorted people by name: " << endl;
+    cout << "\nSorted people by name: " << endl;
     for (const Person &p : sortedNames)
     {
         cout << p.name << ", " << p.age << endl;
     }
 
-    cout << "Sorted by age, then name: " << endl;
-    for (const Person &p : sortAges)
+    cout << "\nSorted by age, then name: " << endl;
+    for (const Person &p : sortedAges)
     {
         cout << p.name << ", " << p.age << endl;
     }
